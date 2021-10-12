1889e8dff658b1a1e6e3127675d688d963397938
badriamujki <deathlige@gmail.com>
2021-10-12 19:22:54 +0800

    first commit

---
+++ JavaBasic.java
@@ -0,0 +1,20 @@
+public class JavaBasic {
+
+    public static void main(String[] args) {
+
+        String first_name, last_name, hobi, phone_number, address;
+        int age;
+
+        first_name = "Badria";
+        last_name = "Mujki";
+        age = 20;
+        phone_number = "085239150943";
+        address = "Lombok";
+        hobi = "baca";
+
+        System.out.println("Name    :" + first_name + last_name + "\n" + "Age     :" + age + "Tahun" + "\n"
+                + "Phone   :" + phone_number + "\n" + "Addres  :" + address + "\n" + "Hobi    :" + hobi + "\n");
+
+    }
+
+}
